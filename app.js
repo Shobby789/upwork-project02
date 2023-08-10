d2f61142c9268a8191c46769ee61b4b93788bf68
Shobby789 <smshoaib2001@gmail.com>
2023-08-10 14:09:05 +0500

    changed slider images

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-const getItems = async () => {
-  try {
-    await fetch("/api", {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        localStorage.setItem("images", data);
-        console.log(data.data);
-      });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 function first() {
   document.getElementById("leftSlider").src = "./images/2.jpg";
   document.getElementById("slideImage").src = "./images/girl-2.jpg";
@@ -32,8 +17,29 @@ function fourth() {
   document.getElementById("leftSlider").src = "./images/1.jpg";
   document.getElementById("slideImage").src = "./images/girl-1.jpg";
 }
+// setInterval(first, 3000);
+// setInterval(second, 6000);
+// setInterval(third, 12000);
+// setInterval(fourth, 18000);
 
-setInterval(first, 3000);
-setInterval(second, 6000);
-setInterval(third, 12000);
-setInterval(fourth, 18000);
+const serverImages = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhtnbEFrnCD7nzIn3Na069EoiRxzHgDXSSug&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQAyBCCtRAuTx5HM_Z672F45i3l_EoiSTw8BBAuqUQoHDji9kffZQUe5Gmo5woXfYtoV8&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLGelHqF1z6ZbVqKWcjB5vHrdEF561Wqq1Lvob8i-LHLv1UEWO8ByGU85ZkSVVvvHOTD8&usqp=CAU",
+  "https://image.winudf.com/v2/image1/bmV0LndsbHBwci5ib3lzX3Byb2ZpbGVfcGljdHVyZXNfc2NyZWVuXzBfMTY2NzUzNzYxN18wOTk/screen-0.webp?fakeurl=1&type=.webp",
+];
+
+function changeImages() {
+  localStorage.setItem("images", JSON.stringify(serverImages));
+  const imageUrls = JSON.parse(localStorage.getItem("images"));
+
+  let count = 0;
+  setInterval(() => {
+    document.getElementById("rightImageSlider").src = imageUrls[count];
+    document.getElementById("leftSlider").src = imageUrls[count];
+    count++;
+    if (count > 3) {
+      count = 0;
+    }
+  }, 3500);
+}
